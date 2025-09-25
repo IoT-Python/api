bdd726944ceb636951341a8d5156b648a20ef07b
lorenestauffer <lorene.stauffer@campus-centre.fr>
2025-09-25 14:09:48 +0200

    Rajout TOPIC dans varaible d'environnement

--- recupInfo.py
+++ recupInfo.py
@@ -6,22 +6,25 @@ import os
 
 load_dotenv()
 
+MQTT_TOPIC=os.getenv('MQTT_TOPIC')
+MQTT_USER=os.getenv('MQTT_USER')
+MQTT_PASSWORD=os.getenv('MQTT_PASSWORD')
+MQTT_HOST=os.getenv('MQTT_HOST')
+MONGODB=os.getenv('MONGODB')
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("v3/+/devices/+/up")
+    client.subscribe(MQTT_TOPIC)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     collection.insert_one(json.loads(msg.payload))
 
-MQTT_USER=os.getenv('MQTT_USER')
-MQTT_PASSWORD=os.getenv('MQTT_PASSWORD')
-MQTT_HOST=os.getenv('MQTT_HOST')
-MONGODB=os.getenv('MONGODB')
+
 
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
